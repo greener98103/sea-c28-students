a27ed64d84b6eb2b4c9d005c524834b6a509faa6
greener <greener@uw.edu>
2015-03-12 19:26:50 -0700

    update the dictonary and add conditonals for whitespaces rot13.py

--- Students/RichardGreen/weekone/rot13.py
+++ Students/RichardGreen/weekone/rot13.py
@@ -2,40 +2,38 @@ def rot13(x):
   import string
   #load in a string
   x = str(x)
-#make it lowercase
-
-  rot13_ids = {'a' : 'n', 'b' : 'o', 'c' : 'p' , 'd' : 'q', 'e' : 'r', 'f' : 's', 'g' : 't', 'h' : 'u', 'i' : 'v', 'j' : 'w','k' : 'x','l' : 'y','m' : 'z','n' : 'a','o' : 'b', 'p' : 'c','q' : 'd','r' : 'e','s' : 'f','t' : 'g','u' : 'h','v' : 'i','w' : 'j','x' : 'k','y' : 'l','z' : 'm'}
-
-  # add capitalized characters
-
-  #{'a' : 'n', 'b' : 'o', 'c' : p , 'd' : q, 'e' : r, 'f' : s, 'g' : t, 'h' :u, 'i' : v, 'j' : w,'k' : x,'l' : y,'m' : z,'n' : a,'o' : b, 'p' : c,'q' : d,'r' : e,'s' : f,'t' : g,'u' : h,'v' : i,'w' : j,'x' : k,'y' : l,'z' : m}
-
+#Create a dictonary that handles the conversion/encryption of letters. Add a place at the end for whitespace
+  rot13_ids = {'a' : 'n', 'b' : 'o', 'c' : 'p' , 'd' : 'q', 'e' : 'r', 'f' : 's', 'g' : 't', 'h' : 'u', 'i' : 'v', 'j' : 'w','k' : 'x','l' : 'y','m' : 'z','n' : 'a','o' : 'b', 'p' : 'c','q' : 'd','r' : 'e','s' : 'f','t' : 'g','u' : 'h','v' : 'i','w' : 'j','x' : 'k','y' : 'l','z' : 'm','A' : 'N', 'B' : 'O', 'C' : 'P' , 'D' : 'Q', 'E' : 'R', 'F' : 'S', 'G' : 'T', 'H' : 'U', 'I' : 'V', 'J' : 'W','K' : 'X','L' : 'Y','M' : 'Z','N' : 'A','O' : 'B', 'P' : 'C','Q' : 'D','R' : 'E','S' : 'F','T' : 'G','U' : 'H','V' : 'I','W' : 'J','X' : 'K','Y' : 'L','Z' : 'M', ' ': ' '}
 
   #create an empty list to write our converted, concatenated results to
   s = ''
   results = list(s)
 
-  #make it lowercase
-  #split letters up
+ #split letters up
   split_list = list(x)
-  # convert values to rot13 values
+  # interate through each character
 
   for id in split_list:
-    if id.isspace():
+# convert values to rot13 values
+    if id in string.whitespace:
       results += "".join(id)
     if id in string.punctuation:
       results += "".join(id)
-
     else:
       results += "".join(rot13_ids[id])
-
   return results
 
 
 if __name__ == '__main__':
   print(rot13('ab'))
-  print(rot13('ba'))
-  print(rot13('abcdefghi'))
+  print(rot13(' R'))
+  #lets test punctuation
+  print(rot13('%$abcdefghi'))
+  #lets test lowercase and punctuation
   print(rot13('rich%'))
-  #print(rot13('Rich'))
-  #print(rot13('Green '))
+  #lets test upper and lower case
+  print(rot13('Rich '))
+  #lets test upper and lower case again
+  print(rot13('Code'))
+  #lets test all upper case
+  print(rot13('FELLOWS'))
