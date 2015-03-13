75128a68203e0a7dd9084bc10bf7cbfab5734f2f
Richard Green <greener@uw.edu>
2015-03-13 12:29:57 -0700

    added name error function to breakme.py

---
+++ Students/RichardGreen/weekone/breakme.py
@@ -0,0 +1,6 @@
+'''This file outlines examples four four different error functions. NameError, TypeError, SyntaxError, AttributeError.
+Their inputs are either an integer, str, or object. The functions are written do not return anything but print statements.Instead of actual print statements they produce errors.'''
+
+def NameE(a):
+# here we get a name error because we are attempting to access a variable that doesnt exist
+    print(y)
